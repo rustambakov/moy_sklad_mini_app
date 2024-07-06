e8c2fb3be3e5aba3a77a170f4265280275a0734a
Rustam Testov <admin@vitaturk.ru>
2024-07-06 18:39:09 +0300

    express

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express'
 
 const app = express()
+
 let port = process.env.PORT || 80
+let botToken = process.env.BOT_TOKEN
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
