4b9053907b2c2e71d54cd0b68d94fa6b44ddd123
Rustam <rustam.bakov@gmail.com>
2024-07-13 18:30:30 +0300

    7

--- src/utilities.js
+++ src/utilities.js
@@ -8,3 +8,5 @@ export function checkHash(hash, initData, botToken) {
         .update(dataCheckString)
         .digest("hex");
 }
+
+//query_id=AAF-OItjAAAAAH44i2Omoagn&user=%7B%22id%22%3A1670068350%2C%22first_name%22%3A%22R%22%2C%22last_name%22%3A%22%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1720884133&hash=be2c19d680d966e600e08d754b2b213f090420092be62612c0887e3995e5e3c0
